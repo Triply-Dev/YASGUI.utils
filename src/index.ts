a9ae13dcfd613fdbcf2efde3e6551cb4dbdd9cd8
Laurens Rietveld <laurens.rietveld@gmail.com>
2017-12-17 14:12:26 +0100

    added svg string utils again

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-
-import _Storage from './Storage';
+import _Storage from "./Storage";
 export var Storage = _Storage;
+
+export function drawSvgStringAsElement(svgString: string) {
+  if (svgString && svgString.indexOf("<svg") == 0) {
+    //no style passed via config. guess own styles
+    var parser = new DOMParser();
+    var dom = parser.parseFromString(svgString, "text/xml");
+    var svg = dom.documentElement;
+
+    var svgContainer = document.createElement("div");
+    svgContainer.className = "svgImg";
+    svgContainer.appendChild(svg);
+    return svgContainer;
+  }
+}

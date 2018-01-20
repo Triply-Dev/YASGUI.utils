caa7c426aec5574449a50000a1a193842b432ade
Laurens Rietveld <laurens.rietveld@gmail.com>
2018-01-20 17:52:40 +0100

    added class manipulation funcs

--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,25 @@ export function drawSvgStringAsElement(svgString: string) {
     return svgContainer;
   }
 }
+export function hasClass(el: Element, className: string) {
+  if (!el) return;
+  if (el.classList) return el.classList.contains(className);
+  else
+    return !!el.className.match(new RegExp("(\\s|^)" + className + "(\\s|$)"));
+}
+
+export function addClass(el: Element, ...classNames: string[]) {
+  if (!el) return;
+  for (const className of classNames) {
+    if (el.classList) el.classList.add(className);
+    else if (!hasClass(el, className)) el.className += " " + className;
+  }
+}
+export function removeClass(el: Element, className: string) {
+  if (!el) return;
+  if (el.classList) el.classList.remove(className);
+  else if (hasClass(el, className)) {
+    var reg = new RegExp("(\\s|^)" + className + "(\\s|$)");
+    el.className = el.className.replace(reg, " ");
+  }
+}
